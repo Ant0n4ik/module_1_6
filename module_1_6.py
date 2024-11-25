af681ef9ae59e5db2a7787eb9ed8a6b97c302965
Антон <katrinnnn.tgr@gmail.com>
2024-11-25 19:09:13 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Антон': 2024}
+print(my_dict)
+my_dict['Екатерина'] = 2003
+print(my_dict)
+my_dict = {'Антон': 2024, 'Кирилл': 2005, 'Артём': 2005}
+print(my_dict)
+del my_dict['Артём']
+print(my_dict)
+my_set = {1, 2, 3, 1, 2, 4, True, 'Антотн'}
+print(my_set)
+my_set = {1, 2, 3, 1, 2, 4, True, 'Антотн', (1, 5, 8)}
+print(my_set)
+my_set.discard(2)
+print(my_set)
